73e4f14274ce20ffc71b5a90ec08f80a9be5b685
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:47:02 +1300

    Converted to PSR-2

--- code/LiveChatMessage.php
+++ code/LiveChatMessage.php
@@ -3,15 +3,16 @@
 /**
  * A single message sent to an individual
  */
-class LiveChatMessage extends DataObject {
-	public static $db = array(
-		'Message' => 'Text',
-		"FromIP" => 'Text',
-		"FromName" => 'Text', // name for anonymous conversations. Must not be numeric. 
-		'Read' => 'Boolean'
-	);
-	public static $has_one = array(
-		"From" => "Member",
-		"To" => "Member"
-	);
+class LiveChatMessage extends DataObject
+{
+    public static $db = array(
+        'Message' => 'Text',
+        "FromIP" => 'Text',
+        "FromName" => 'Text', // name for anonymous conversations. Must not be numeric. 
+        'Read' => 'Boolean'
+    );
+    public static $has_one = array(
+        "From" => "Member",
+        "To" => "Member"
+    );
 }
